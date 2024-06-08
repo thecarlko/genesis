8c7a83d0cd481dad084baefcc0af719204243a93
Carl Omondi <carlkomondi@gmail.com>
2024-06-08 11:04:34 -0500

    Added wishes servers

---
+++ src/routes/api/wishtree/+server.ts
@@ -0,0 +1,59 @@
+
+
+import { initializeApp } from "firebase/app";
+import { addDoc, collection, doc, getDoc, getDocs, getFirestore, limit, query } from "firebase/firestore";
+import type { RequestHandler } from "@sveltejs/kit";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBjSnFXEXgsx1uww1PmC45yOaQcDn8uK1s",
+    authDomain: "genesislms.firebaseapp.com",
+    projectId: "genesislms",
+    storageBucket: "genesislms.appspot.com",
+    messagingSenderId: "513101123280",
+    appId: "1:513101123280:web:ada4def8b5ab8811f9d8d5",
+    measurementId: "G-6722B6QG6Q"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+
+export const GET: RequestHandler = async({ }) => {
+
+    try {
+        const data = query(collection(database, "wishes"), limit(120));
+        const snapshot = await getDocs(data);
+    
+        const wishes : any[] = [];
+    
+        snapshot.forEach((doc) => {
+            wishes.push({ id: doc.id, ...doc.data() });
+        });
+
+        return new Response(JSON.stringify(wishes), {
+            headers: { 'Content-Type': 'application/json' },
+            status: 200
+        });
+    } catch (error) {
+        console.error('Error retrieving documents:', error);
+        return new Response(JSON.stringify({ error: 'Failed to retrieve documents' }), {
+          headers: { 'Content-Type': 'application/json' },
+          status: 500
+        });
+    }
+
+
+
+}
+
+export const POST: RequestHandler = async ({ request }) => {
+
+    const { message } = await request.json();
+
+    const document = await addDoc(collection(database, "wishes"), { message: message });
+    console.log(document)
+
+    return new Response("Successfully added document");
+}
+
+
---
+++ src/routes/inventory/+page.svelte
@@ -0,0 +1,55 @@
+
+
+
+
+<h1>Welcome to the inventory</h1>
+
+
+
+<section id="results">
+
+
+    <div class="list">
+        <div class="">
+            <span>Title</span>
+            <span>Neal Shusterman</span>
+            <span>9781442472426</span>
+            <span>24</span>
+        <div/>
+
+        <a href="/inventory/flowers">
+            <span>Arc of a Scythe</span>
+            <span>Neal Shusterman</span>
+            <span>9781442472426</span>
+            <span>24</span>
+        </a>
+    </div>
+
+</section>
+
+
+<style lang="scss">
+
+    @use "$lib/styles/variables" as app;
+
+
+    div.list {
+        background-color: app.$color-background;
+        filter: app.$drop-shadow;
+        max-width: 920px;
+
+        margin: 0px auto;
+    }
+
+    div.list > * {
+        display: grid;
+        gap: 0px 1rem;
+        grid-template-rows: 1fr;
+        grid-template-columns:  3fr 2fr max-content 3rem;
+    }
+
+    div.list > * > span:nth-child(3) { text-align: center; }
+    div.list > * > span:nth-child(4) { text-align: center; }
+</style>
+
+
